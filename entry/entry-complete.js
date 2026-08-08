898648397f804d6d2e942f850f044c637d29c147
Samuel Fernández Amorós <samuel84fa@gmail.com>
2026-08-08 16:00:50 +0200

    Refactor plugin imports and mounting in entry-complete.js
    Updated import paths for Swap and MultiDrag plugins and mounted them in a single call.

--- entry/entry-complete.js
+++ entry/entry-complete.js
@@ -1,8 +1,8 @@
 import Sortable from './entry-defaults.js';
-import Swap from '../plugins/Swap';
-import MultiDrag from '../plugins/MultiDrag';
+import Swap from '../plugins/Swap/index.js';
+import MultiDrag from '../plugins/MultiDrag/index.js';
 
-Sortable.mount(new Swap());
-Sortable.mount(new MultiDrag());
+// Mount plugins in a single call
+Sortable.mount(new Swap(), new MultiDrag());
 
 export default Sortable;
