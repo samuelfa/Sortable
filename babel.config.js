008031e4dafd5eca36ebc6da595b0a3be6d2c904
Samuel Fernández Amorós <samuel84fa@gmail.com>
2026-08-08 15:33:02 +0200

    Refactor babel.config.js to use ES module syntax

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,28 @@
-module.exports = function(api) {
-	api.cache(true);
+export default function (api) {
+  /* Cache the configuration based on NODE_ENV */
+  api.cache(true);
 
-	let presets;
+  let presets;
 
-	if (process.env.NODE_ENV === 'es') {
-		presets = [
-			[
-				"@babel/preset-env",
-				{
-					"modules": false
-				}
-			]
-		];
-	} else if (process.env.NODE_ENV === 'umd') {
-		presets = [
-			[
-				"@babel/preset-env"
-			]
-		];
-	}
+  if (process.env.NODE_ENV === 'es') {
+    presets = [
+      [
+        '@babel/preset-env',
+        {
+          modules: false,
+        },
+      ],
+    ];
+  } else if (process.env.NODE_ENV === 'umd') {
+    presets = [
+      [
+        '@babel/preset-env',
+      ],
+    ];
+  }
 
-	return {
-		plugins: ['@babel/plugin-transform-object-assign'],
-		presets
-	};
-};
+  return {
+    plugins: ['@babel/plugin-transform-object-assign'],
+    presets,
+  };
+}
