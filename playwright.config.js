460fb12a2ccf22fc248b14a81c8f25de5100b982
Samuel Fernández Amorós <samuel84fa@gmail.com>
2026-08-08 23:02:13 +0200

    Reduce CI retries from 2 to 1

--- playwright.config.js
+++ playwright.config.js
@@ -1,13 +1,14 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
+	maxFailures: 4,
 	testDir: './tests',
 	/* Run tests in files in parallel to speed up execution */
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code */
 	forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
-	retries: process.env.CI ? 2 : 0,
+	retries: process.env.CI ? 1 : 0,
 	/* Opt out of parallel tests on CI to ensure stability */
 	workers: process.env.CI ? 1 : undefined,
 
