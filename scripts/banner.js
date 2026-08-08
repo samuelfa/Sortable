b53cafe8cdf5ae57ad7543beb92a8401af5d7c76
Samuel FA <samuel84fa@gmail.com>
2026-08-08 15:31:59 +0200

    fix import pkg in banner.js

--- scripts/banner.js
+++ scripts/banner.js
@@ -1,9 +1,10 @@
-import { version } from '../package.json' with { type: 'json' };
+import pkg from '../package.json' with { type: 'json' };
 
 export default `/**!
- * Sortable ${ version }
+ * Sortable ${ pkg.version }
  * @author	RubaXa           <trash@rubaxa.org>
  * @author	owenm            <owen23355@gmail.com>
  * @author	Samuel Fernández <samuel84fa@gmail.com>
  * @license MIT
  */`;
+
